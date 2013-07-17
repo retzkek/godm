4460f0a99db3a4ab679cf8a17662cb9269a65fc0
Kevin Retzke <retzkek@gmail.com>
2013-07-17 10:36:42 -0500

    help text generated from command map

--- godm/main.go
+++ godm/main.go
@@ -6,26 +6,6 @@ import (
 	"os"
 )
 
-func printHelp(args []string) {
-	fmt.Printf("\nusage: %s cmd [args]\n", args[0])
-	fmt.Printf(`commands:
-	help [COMMAND]
-		Show help on the program or a command.
-	store FILE [TAGS]
-		Store file with optional comma-delimited tags.
-	get FILE
-		Get file by name.
-	smite FILE
-		Delete file.
-	ls QUERY
-		List all files with name matching QUERY.
-	tag FILE TAGS
-		Add TAGS (comma-delimited) to FILE.
-	
-	search TERMS
-		Search for a file.`)
-}
-
 type CommandFunc func([]string)
 
 type Command struct {
@@ -34,19 +14,29 @@ type Command struct {
 	Function  CommandFunc
 }
 
+func printHelp(args []string, cmds map[string]Command) {
+	fmt.Printf("\nusage: %s COMMAND [ARGS]\n", args[0])
+	fmt.Printf("commands:\n")
+	fmt.Printf("\thelp [COMMAND]\n")
+	fmt.Printf("\t\tShow this help or get detailed help on a command.\n")
+	for _, c := range cmds {
+		if c.Function != nil {
+			fmt.Printf("\t%s\n", c.Usage)
+			fmt.Printf("\t\t%s\n", c.BriefHelp)
+		}
+	}
+}
+
 func main() {
 	db := godm.Db{"localhost:27017", "test"}
+	// 'help' is not in the map as it requires
+	// the contents of the map.
 	var cmds = map[string]Command{
-		"help": {"help [COMMAND]",
-			"Show help on the program or a command.",
-			func(args []string) {
-				fmt.Printf("\nusage: %s cmd [args]\n", args[0])
-			}},
 		"store": {"store FILE [TAGS]",
 			"Store file with optional comma-delimited tags.",
 			func(args []string) {
 				if len(args) < 3 {
-					printHelp(os.Args)
+					fmt.Println("Insufficient arguments. 'help' for usage.")
 					os.Exit(1)
 				}
 				if err := db.Store(args[2]); err != nil {
@@ -57,7 +47,7 @@ func main() {
 			"Get file by name.",
 			func(args []string) {
 				if len(args) < 3 {
-					printHelp(os.Args)
+					fmt.Println("Insufficient arguments. 'help' for usage.")
 					os.Exit(1)
 				}
 				if err := db.Get(args[2]); err != nil {
@@ -74,18 +64,20 @@ func main() {
 			"Search for all files matching QUERY.", nil},
 	}
 	if len(os.Args) < 2 {
-		printHelp(os.Args)
+		printHelp(os.Args, cmds)
 		os.Exit(1)
 	}
 	cmd := os.Args[1]
-	if c, ok := cmds[cmd]; ok {
+	if cmd == "help" {
+		printHelp(os.Args, cmds)
+	} else if c, ok := cmds[cmd]; ok {
 		if c.Function != nil {
 			c.Function(os.Args)
 		} else {
 			fmt.Println("not implemented")
 		}
 	} else {
-		printHelp(os.Args)
+		printHelp(os.Args, cmds)
 		os.Exit(1)
 	}
 }
